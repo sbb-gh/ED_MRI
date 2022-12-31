cec814cb86c0e4837f50a1fe3c98ab3c748e4a91
sbb-threadrip16 <stefano.blumberg.17@ucl.ac.uk>
2022-12-31 18:12:18 +0000

    Cleanup simulations.py

--- utils/simulations.py
+++ utils/simulations.py
@@ -1,35 +1,43 @@
 import numpy as np
 from dmipy.signal_models import sphere_models, cylinder_models, gaussian_models
 from dmipy.distributions.distribute_models import SD1WatsonDistributed
-from dmipy.signal_models import gaussian_models
 from dmipy.core.modeling_framework import MultiCompartmentModel
 from dmipy.utils.utils import unitsphere2cart_Nd
 
 
 def noddi_model():
+    """Create NODDI model from https://pubmed.ncbi.nlm.nih.gov/22484410"""
 
     ball = gaussian_models.G1Ball()
     stick = cylinder_models.C1Stick()
     zeppelin = gaussian_models.G2Zeppelin()
     watson_dispersed_bundle = SD1WatsonDistributed(models=[stick, zeppelin])
 
-    # fix parameters and tortuosity constraints
+    # Fix parameters and tortuosity constraints
     watson_dispersed_bundle.set_tortuous_parameter(
         "G2Zeppelin_1_lambda_perp", "C1Stick_1_lambda_par", "partial_volume_0"
     )
-    watson_dispersed_bundle.set_equal_parameter(
-        "G2Zeppelin_1_lambda_par", "C1Stick_1_lambda_par"
-    )
+    watson_dispersed_bundle.set_equal_parameter("G2Zeppelin_1_lambda_par", "C1Stick_1_lambda_par")
     watson_dispersed_bundle.set_fixed_parameter("G2Zeppelin_1_lambda_par", 1.7e-9)
 
     NODDI_mod = MultiCompartmentModel(models=[ball, watson_dispersed_bundle])
-    # fix isotropic diffusivity
+    # Fix isotropic diffusivity
     NODDI_mod.set_fixed_parameter("G1Ball_1_lambda_iso", 3e-9)
 
     return NODDI_mod
 
 
 def noddi(nsamples, acq_scheme):
+    """Create NODDI model and simulate data
+
+    Args:
+        nsamples (int): Number of samples (voxels) to simulate
+        acq_scheme (dmipy.data.saved_acquisition_schemes)
+
+    Returns:
+        Signals_NODDI: Simulated MRI signal
+        Parameters_NODDI: Parameters used to simulate signal
+    """
 
     NODDI_mod = noddi_model()
 
@@ -39,7 +47,7 @@ def noddi(nsamples, acq_scheme):
     f_1 = 1 - f_0
     f_bundle = np.random.uniform(low=0.01, high=0.99, size=nsamples)
 
-    # big parameter vector for simulate_signal
+    # Big parameter vector for simulate_signal
     Parameters_NODDI_dmipy = NODDI_mod.parameters_to_parameter_vector(
         SD1WatsonDistributed_1_SD1Watson_1_mu=mu,
         SD1WatsonDistributed_1_SD1Watson_1_odi=odi,
@@ -48,14 +56,13 @@ def noddi(nsamples, acq_scheme):
         partial_volume_1=f_1,
     )
 
+    print("Simulating NODDI data", flush=True)
     Signals_NODDI = NODDI_mod.simulate_signal(acq_scheme, Parameters_NODDI_dmipy)
     # NODDI_mod.parameter_names
     Signals_NODDI = add_noise(Signals_NODDI)
 
     # add cartesian parameters parameters_NODDI are the parameters to learn
-    Parameters_NODDI_all, Parameters_NODDI = add_cartesian(
-        NODDI_mod, Parameters_NODDI_dmipy
-    )
+    Parameters_NODDI_all, Parameters_NODDI = add_cartesian(NODDI_mod, Parameters_NODDI_dmipy)
 
     Signals_NODDI = Signals_NODDI.astype(np.float32)
     Parameters_NODDI = Parameters_NODDI.astype(np.float32)
@@ -66,36 +73,46 @@ def noddi(nsamples, acq_scheme):
 
 
 def verdict_model():
-    # fixed to verdict value
+    """Create VERDICT Model https://pubmed.ncbi.nlm.nih.gov/25426656"""
+
+    # Fixed to verdict value
     sphere = sphere_models.S4SphereGaussianPhaseApproximation(diffusion_constant=0.9e-9)
     ball = gaussian_models.G1Ball()
     stick = cylinder_models.C1Stick()
-
     VERDICT_mod = MultiCompartmentModel(models=[sphere, ball, stick])
-    # VERDICT_mod.parameter_names
 
-    # verdict limits
+    # VERDICT fix parameter and set ranges
     VERDICT_mod.set_fixed_parameter("G1Ball_1_lambda_iso", 0.9e-9)
-    VERDICT_mod.set_parameter_optimization_bounds(
-        "C1Stick_1_lambda_par", [3.05e-9, 10e-9]
-    )
-    VERDICT_mod.parameter_ranges
+    VERDICT_mod.set_parameter_optimization_bounds("C1Stick_1_lambda_par", [3.05e-9, 10e-9])
 
     return VERDICT_mod
 
 
 def verdict_params_norm(Parameters_VERDICT):
+    """Normalize VERDICT parameters so all on same scale"""
+
     print("Normalizing means of Parameters_VERDICT")
     Parameters_VERDICT[:, 0] = Parameters_VERDICT[:, 0] * (10**5) * 0.5
     Parameters_VERDICT[:, 1] = Parameters_VERDICT[:, 1] * (10**8)
+
     return Parameters_VERDICT
 
 
-def verdict(nsamples, acq_scheme, parameters=None):
+def verdict(nsamples, acq_scheme):
+    """Create VERDICT model and simulate data
+
+    Args:
+        nsamples (int): Number of samples (voxels) to simulate
+        acq_scheme (dmipy.data.saved_acquisition_schemes)
+
+    Returns:
+        Signals_VERDICT: Simulated MRI signal
+        Parameters_VERDICT: Parameters used to simulate signal
+    """
 
     VERDICT_mod = verdict_model()
 
-    # random parameters with sensible upper and lower bounds
+    # Random parameters with sensible upper and lower bounds
     mu = np.random.uniform(low=[0, -np.pi], high=[np.pi, np.pi], size=(nsamples, 2))
     lambda_par = np.random.uniform(low=1e-9, high=10e-9, size=nsamples)  # in m^2/s
     diameter = np.random.uniform(low=0.01e-06, high=20e-06, size=nsamples)
@@ -103,7 +120,7 @@ def verdict(nsamples, acq_scheme, parameters=None):
     f_1 = np.random.uniform(low=0.01, high=0.99 - f_0, size=nsamples)
     f_2 = 1 - f_0 - f_1
 
-    # put into a big parameter vector that can be passed into simulate_signal
+    # Big parameter vector to simulate_signal
     Parameters_VERDICT_dmipy = VERDICT_mod.parameters_to_parameter_vector(
         C1Stick_1_mu=mu,
         C1Stick_1_lambda_par=lambda_par,
@@ -113,25 +130,23 @@ def verdict(nsamples, acq_scheme, parameters=None):
         partial_volume_2=f_2,
     )
 
+    print("Simulating VERDICT data", flush=True)
     Signals_VERDICT = VERDICT_mod.simulate_signal(acq_scheme, Parameters_VERDICT_dmipy)
     Signals_VERDICT = add_noise(Signals_VERDICT)
 
-    # add cartesian parameters parameters_VERDICT are the parameters to learn
-    Parameters_VERDICT_all, Parameters_VERDICT = add_cartesian(
-        VERDICT_mod, Parameters_VERDICT_dmipy
-    )
+    # Add Cartesian parameters
+    _, Parameters_VERDICT = add_cartesian(VERDICT_mod, Parameters_VERDICT_dmipy)
 
     Signals_VERDICT = Signals_VERDICT.astype(np.float32)
     Parameters_VERDICT = Parameters_VERDICT.astype(np.float32)
-    Parameters_VERDICT_all = Parameters_VERDICT_all.astype(np.float32)
-    Parameters_VERDICT_dmipy = Parameters_VERDICT_dmipy.astype(np.float32)
-
     Parameters_VERDICT = verdict_params_norm(Parameters_VERDICT)
 
     return Signals_VERDICT, Parameters_VERDICT
 
 
 def add_noise(data, scale=0.02):
+    """Add Rician noise to data"""
+
     data_real = data + np.random.normal(scale=scale, size=np.shape(data))
     data_imag = np.random.normal(scale=scale, size=np.shape(data))
     data_noisy = np.sqrt(data_real**2 + data_imag**2)
@@ -139,21 +154,33 @@ def add_noise(data, scale=0.02):
     return data_noisy
 
 
-def add_cartesian(model, Parameters_dmipy):
-    # model is dmipy model, Parameters_dmipy is dmipy parameter_vector,
-    # mu_name is the name of the spherical coordinates to convert
-    # find where the mu parameter is, and therefore the index of theta and phi
+def add_cartesian(model, parameters_dmipy):
+    """
+    Args:
+        model: dmipy model
+        parameters_dmipy: dmipy parameter vector
+
+    Returns:
+        parameters_all: All parameters, including spherical and Cartesian
+        parameters_cart_only: Parameters replacing spherical coordinates with Cartesian
+    """
+
+    # mu_{name} is the name of the spherical coordinates to convert
+    # Find mu parameter and therefore the index of theta and phi
     mu_index = [i for i, s in enumerate(model.parameter_names) if "_mu" in s][0]
     theta_phi_index = mu_index, mu_index + 1
-    # convert to cartesian coordinates
-    mu_cartesian = unitsphere2cart_Nd(Parameters_dmipy[:, theta_phi_index])
-    # flip the direction of any cartesian points in the lower half of the sphere
+
+    # Convert to cartesian coordinates
+    mu_cartesian = unitsphere2cart_Nd(parameters_dmipy[:, theta_phi_index])
+
+    # Flip the direction of any cartesian points in the lower half of the sphere
     lower_index = mu_cartesian[:, 2] < 0
     mu_cartesian[lower_index, :] = -mu_cartesian[lower_index, :]
 
-    # add the cartesian coordinates to the parameter array
-    Parameters_all = np.append(Parameters_dmipy, mu_cartesian, axis=1)
-    # remove the spherical coordinates (i.e. "mu") from the main parameter array (this is what you want to learn)
-    Parameters_cart_only = np.delete(Parameters_all, theta_phi_index, axis=1)
+    # Add cartesian coordinates to the parameter array
+    parameters_all = np.append(parameters_dmipy, mu_cartesian, axis=1)
 
-    return Parameters_all, Parameters_cart_only
+    # Remove spherical coordinates ("mu") from the parameter
+    parameters_cart_only = np.delete(parameters_all, theta_phi_index, axis=1)
+
+    return parameters_all, parameters_cart_only
